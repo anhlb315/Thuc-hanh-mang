b69ab3521c4231298f1d84a0a47de66b751262a2
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-10-25 15:30:59 +0700

    Update backbone of code

--- Bài tập thực hành 2/resolver.c
+++ Bài tập thực hành 2/resolver.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
-    
+    if(argc != 3) {
+        printf("Parameters Error\n");
+        return 0;
+    }
+
+    int argv_1 = atoi(argv[1]);
+    switch (argv_1)
+    {
+    case 1:
+        /* code */
+        break;
+    case 2:
+        /* code */
+        break;
+    default:
+        printf("Paraemters Error\n");
+        break;
+    }
     return 0;
 }
