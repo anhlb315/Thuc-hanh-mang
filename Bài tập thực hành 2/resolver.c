9a120029a29452e81b79a4030cdfe854c1b88447
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-10-26 09:34:36 +0700

    Create makefile

--- Bài tập thực hành 2/resolver.c
+++ Bài tập thực hành 2/resolver.c
@@ -9,6 +9,13 @@
 struct hostent *host_info;
 struct in_addr *address;
 
+int is_valid_ip_address(char *ipAddress)
+{
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+    return result != 0;
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 3)
@@ -27,7 +34,21 @@ int main(int argc, char **argv)
         printf("%s has address %s\n", argv[2], inet_ntoa(*address));
         break;
     case 2:
-        /* code */
+        if (!is_valid_ip_address(argv[2]))
+        {
+            printf("%s is not valid\n", argv[2]);
+            return 0;
+        }
+
+        struct sockaddr_storage addr;
+        socklen_t addr_len = sizeof(addr);
+        char buffer[NI_MAXHOST];
+        int err = getnameinfo((struct sockaddr *)&addr, addr_len, argv[2], 1024, 0, 0, 0);
+        if (err != 0)
+        {
+            printf("failed to convert address to string (code=%d)\n", err);
+        }
+        printf("Remote address: %s\n", argv[2]);
         break;
     default:
         printf("Paraemters Error\n");
