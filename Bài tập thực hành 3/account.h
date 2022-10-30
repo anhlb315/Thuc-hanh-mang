1322183519ee80cfb0edd64fa77bec56b82d2f0a
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-10-30 22:50:15 +0700

    Copy code from Week 1

---
+++ Bài tập thực hành 3/account.h
@@ -0,0 +1,23 @@
+typedef struct _Account {
+    char username[30];
+    char password[30];
+    int status;
+    int is_signed_in;
+    struct _Account *next;
+} Account;
+
+Account *create_new_account(char *username, char *password, int status);
+Account *add_account(Account *account, char *username, char *password, int status);
+int check_user(Account *account, char *username);
+int check_password(Account *account, char *password);
+int check_blocked(Account *account, char *username);
+int check_signed_in(Account *account, char *username);
+Account *read_account(Account *account);
+Account *register_account(Account *account);
+void update_file(Account *account);
+void sign_in(Account *account);
+void search(Account *account);
+void sign_out(Account *account);
+void free_list(Account *account);
+void activate(Account *account);
+void change_password(Account * account);
---
+++ Bài tập thực hành 3/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "account.h"
+
+int main() {
+    int menu;
+    Account *acc = NULL;
+    acc = read_account(acc);
+    while (1)
+    {
+        printf("\nUSER MANAGEMENT PROGRAM\n");
+        printf("-----------------------------------\n");
+        printf("1. Register\n");
+        printf("2. Activate\n");
+        printf("3. Sign In\n");
+        printf("4. Search\n");
+        printf("5. Change Password\n");
+        printf("6. Sign Out\n");
+        printf("Your choice (1-6, other to quit):\n");
+        scanf("%d", &menu); fflush(stdin);
+        switch (menu) {
+        case 1:
+            acc = register_account(acc);
+            break;
+        case 2:
+            activate(acc);
+            break;
+        case 3:
+            sign_in(acc);
+            break;
+        case 4:
+            search(acc);
+            break;
+        case 5:
+            change_password(acc);
+            break;
+        case 6:
+            sign_out(acc);
+            break;
+        default:
+            free_list(acc);
+            printf("Exit program...\n");
+            exit(0);
+        }
+    }
+}
