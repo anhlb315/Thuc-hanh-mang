daed2d88ec0b9ecb9e079cf819552878e9f5b118
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-10-30 22:56:31 +0700

    Create resolver.h .c

---
+++ Bài tập thực hành 3/resolver.c
@@ -0,0 +1,4 @@
+#include "resolver.h"
+
+void print_domain_name(Account* account);
+void print_ip_address(Account* account);
---
+++ Bài tập thực hành 3/resolver.h
@@ -0,0 +1,4 @@
+#include "account.h"
+
+void print_domain_name(Account* account);
+void print_ip_address(Account* account);
